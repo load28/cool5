1891a2b696902fefe5196e02fb54e93515e0517a
load28 <tjalsdud89@naver.com>
2023-12-12 22:02:24 +0900

    refactoring code

--- src/components/restaurant-of-friends/hooks/useRestauranstServerData.ts
+++ src/components/restaurant-of-friends/hooks/useRestauranstServerData.ts
@@ -1,13 +1,27 @@
 import { useQuery } from 'react-query';
-import useSupabaseQuery from '../../../hooks/useSupabaseQuery';
+import useSupabaseClient from '../../../hooks/useSupabaseClient';
 import { QueryKeys } from '../../../query-client';
 import { Restaurant } from '../models';
-import { list } from './test-data';
 
 const useRestaurantServerData = () => {
-  useSupabaseQuery();
-  const { data, isLoading } = useQuery<Restaurant[]>(QueryKeys.RESTAURANTS, () => {
-    return list;
+  const supabase = useSupabaseClient();
+  const { data, isLoading } = useQuery<Restaurant[]>(QueryKeys.RESTAURANTS, async () => {
+    const { data, error } = await supabase.from('post').select('*');
+
+    if (error) {
+      return [];
+    }
+
+    return data.map((item) => {
+      return {
+        id: item.id,
+        title: item.title,
+        authorId: item.author_id,
+        description: item.description,
+        score: item.score,
+        tag: [],
+      };
+    });
   });
 
   return {
