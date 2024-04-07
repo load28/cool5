804dd16ac9f8eca1a46625971550177e1631c077
load28 <tjalsdud89@naver.com>
2024-04-07 20:05:47 +0900

    feat(ui): Update constant name to MAX_SCORE_VALUE in ListItem component

--- src/components/restaurant-of-friends/ui/ListItem.tsx
+++ src/components/restaurant-of-friends/ui/ListItem.tsx
@@ -4,7 +4,7 @@ import './ListItem.scss';
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@components/ui/card.tsx';
 import { Star } from 'lucide-react';
 
-const MAC_SCORE = 5;
+const MAX_SCORE_VALUE = 5;
 
 const ListItem: React.FC<Restaurant> = (restaurant) => {
   return (
@@ -13,7 +13,7 @@ const ListItem: React.FC<Restaurant> = (restaurant) => {
         <CardHeader>
           <CardTitle>{restaurant.title}</CardTitle>
           <CardDescription className="flex gap-1">
-            {Array.from({ length: MAC_SCORE }).map((_, index) => {
+            {Array.from({ length: MAX_SCORE_VALUE }).map((_, index) => {
               return <Star key={index} size="16px" fill={restaurant.score >= ++index ? '#ffce4b' : '#bac3bd'} strokeWidth={0} />;
             })}
           </CardDescription>
